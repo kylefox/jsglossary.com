89ae36409bb6f71ca14b66ff583873880b02f414
Kyle Fox <kyle.fox@gmail.com>
2016-02-07 10:59:04 -0700

    don't worry about styles for now

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,19 +10,6 @@ import {WordListComponent} from './word-list.component';
         <p>Because the JavaScript world can be confusing.</p>
         <input type="search" autofocus placeholder="Search for React, Angular, Grunt, etc.">
       </header>
-      <word-list></word-list>`,
-    styles: [`
-      header {
-        text-align: center;
-        padding: 4rem 0;
-      }
-
-      h1 {
-        font-size: 2rem;
-        margin: 0 0 1rem 0;
-        color: #2D6ED8;
-        font-weight: 100;
-      }
-    `]
+      <word-list></word-list>`
 })
 export class AppComponent { }
